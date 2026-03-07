5cf65dae5d434b46624eaeb2a9e300ac61680a13
spyisme <amooraymanh730072@gmail.com>
2026-03-08 01:16:38 +0200

    ss

---
+++ Skrew/src/main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 600, 400 }), "SFML Works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
